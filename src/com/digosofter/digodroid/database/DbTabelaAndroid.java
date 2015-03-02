0f87671224100e9fb0b77eb8896813c21cc700f0
Rodrigo Vieira <rodrigo.conspena@gmail.com>
2015-03-02 17:28:39 -0300

    5.0.0
    - Inclusão de método perdido na reestruturação dos projetos que abre a
    tela de cadastro da tabela.

--- src/com/digosofter/digodroid/database/DbTabelaAndroid.java
+++ src/com/digosofter/digodroid/database/DbTabelaAndroid.java
@@ -42,6 +42,25 @@ public abstract class DbTabelaAndroid extends DbTabela {
     }
   }
 
+  public void abrirActCadastro(Activity actPai) {
+
+    Intent objIntent;
+
+    try {
+
+      AppAndroid.getI().setTblSelec(this);
+
+      objIntent = new Intent(actPai, ActConsulta.class);
+      actPai.startActivityForResult(objIntent, 0);
+    }
+    catch (Exception ex) {
+
+      new ErroAndroid(AppAndroid.getI().getStrTextoPadrao(123), ex);
+    }
+    finally {
+    }
+  }
+
   public void abrirActConsulta(Activity actPai) {
 
     Intent objIntent;
