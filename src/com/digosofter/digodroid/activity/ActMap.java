398c6df7c348404bc568a917f15a0434a74d706f
Rodrigo Vieira <rodrigo.conspena@gmail.com>
2015-04-20 13:48:06 -0300

    6.8.10
    - Implementação de activity para mapas.

---
+++ src/com/digosofter/digodroid/activity/ActMap.java
@@ -0,0 +1,12 @@
+package com.digosofter.digodroid.activity;
+
+import com.digosofter.digodroid.R;
+
+public class ActMap extends ActMain {
+
+  @Override
+  protected int getIntLayoutId() {
+
+    return R.layout.act_map;
+  }
+}
